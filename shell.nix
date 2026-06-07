4669ca03e642431ead0740182756ed46586e46e3
bartolor <ryan.bartolomeo@grenoble-inp.org>
2026-06-07 15:38:56 +0200

    Ajout des fichiers sympa

---
+++ shell.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+
+  buildInputs = [
+    pkgs.clang
+    pkgs.gnumake
+    pkgs.ncurses
+    pkgs.rustup
+		pkgs.tree-sitter
+		pkgs.nodejs
+		pkgs.python313Packages.python-lsp-server
+  ];
+
+}
